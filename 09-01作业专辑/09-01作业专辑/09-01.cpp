071086488c3206ed9f16a3a1d0d531373013f977
sis-shen <141546659+sis-shen@users.noreply.github.com>
2023-09-01 16:49:12 +0800

    09-01.cpp

---
+++ 09-01作业专辑/09-01作业专辑/09-01.cpp
@@ -0,0 +1,208 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n))
+//	{
+//		//上n行
+//		int i = 0;
+//		int j = 0;
+//		for (i = 0; i < n; i++)
+//		{
+//			for (j = 0; j < n - i; j++)
+//			{
+//				printf("  ");
+//			}
+//			for (j = 0; j <= i; j++)
+//			{
+//				printf("*");
+//			}
+//			printf("\n");
+//		}
+//
+//
+//
+//		//下n+1行
+//		for (i = 0; i < n+1; i++)
+//		{
+//			for (j = 0; j <  i; j++)
+//			{
+//				printf("  ");
+//			}
+//			for (j = 0; j <=n+1- i; j++)
+//			{
+//				printf("*");
+//			}
+//			printf("\n");
+//		}
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int score = 0;
+//	int count = 0;
+//	int min = 100;
+//	int max = 0;
+//	int sum = 0;
+//	while (scanf("%d", &score) == 1)
+//	{
+//		count++;
+//		if (score > max)
+//		{
+//			max = score;
+//		}
+//		if (score < min)
+//		{
+//			min = score;
+//		}
+//
+//		sum += score;
+//		if (count == 7)
+//		{
+//			printf("%.2lf\n", (sum - min - max) / 5.0);
+//			count = 0;
+//			max = 0;
+//			min = 100;
+//		}
+//
+//	}
+//	return 0;
+//}
+
+//void find_single_dog(int sz, char arr[], int* pd1, int* pd2)
+//{
+//	*pd1 = 0;
+//	*pd2 = 0;
+//	int ret = 0;
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		ret ^= arr[i];
+//
+//	}
+//	int pos = 0;
+//	for (pos = 0; pos < 32; pos++)
+//	{
+//		if (((ret >> pos) & 1) == 1)
+//		{
+//			break;
+//		}
+//	}
+//	for (i = 0; i < sz; i++)
+//	{
+//		if (((arr[i] >> pos) & 1) == 1)
+//		{
+//			*pd1 ^= arr[i];
+//		}
+//		else
+//		{
+//			*pd2 ^= arr[i];
+//		}
+//	}
+//
+//
+//}
+//
+//
+//
+//
+//int main()
+//{	
+//	//找出两个单身狗
+//	char arr[] = { 1,2,3,4,5,1,2,3,4,6 };
+//
+//	//分组
+//	//1.所有数字异或
+//	//2.找出异或结果数字中哪一位为1 -n
+//	//3.以第n位为1分一族 第n位为0 分一族
+//
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int dog1 = 0;
+//	int dog2 = 0;
+//
+//	find_single_dog(sz, arr, &dog1, &dog2);
+//
+//	printf("%d %d", dog1, dog2);
+//
+//	return 0;
+//}
+
+#include <stdlib.h>
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+
+enum Status {
+	VALID,
+	INVALID,
+};
+
+enum Status sta = INVALID;
+int my_atoi(const char* str )
+{
+	int flag = 1;
+	assert(str);
+	if (*str == '\0')
+		return 0;
+	while (isspace(*str))
+	{
+		str++;
+	}
+	if (*str == '+')
+	{
+		flag = 1;
+		str++;
+	}
+	else if(*str == '-')
+	{
+		flag = -1;
+		str++;
+	}
+	long long ret = 0;
+	while (*str)
+	{
+		if (isdigit(*str))
+		{
+			ret = ret * 10 + flag * (*str - '0');
+			//判断越界
+			if (ret > INT_MAX || ret < INT_MIN)
+			{
+				return 0;
+			}
+			str++;
+		}
+		else
+		{
+			return (int)ret;
+		}
+
+	}
+
+
+	sta = VALID;
+	return (int) ret ;
+}
+
+
+
+int main()
+{
+	char arr[20] = "-123abc456";
+	int ret = my_atoi(arr);
+	if (sta == INVALID)
+	{
+		printf("非法返回:%d\n", ret);
+	}
+	else
+	{
+		printf("合法返回:%d\n", ret);
+	}
+	printf("%d", ret);
+	return 0;
+}
